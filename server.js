963f198adcebb686edbf38dc7caa68ba5914f9eb
shreyshah173 <122092519+shreyshah173@users.noreply.github.com>
2024-07-29 22:24:28 +0530

    basic find and filter

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
-import { ApolloServer,gql } from "apollo-server";
+import { ApolloServer, gql } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { quotes, users } from './fakedb.js';
 
 const typeDef = gql`
     type Query {
-        hello: String
+        users: [User]
+        quotes: [Quote]
+        user(id: ID!): User
+        iquote(by: ID!): [Quote]
+    }
+    
+    type User { 
+        id: ID!
+        firstName: String
+        lastName: String
+        email: String
+        quotes: [Quote]
+    }
+
+    type Quote {
+       name: String
+       by: String 
     }
 `
 
 const resolvers = {
     Query: {
-        hello: () => "Hello World"
+        users: () => users,
+        // user: (_,args)=> users.find(user => user.id === args.id),
+        user: (_,{id})=> users.find(user => user.id === id),
+        quotes: () => quotes,
+        iquote: (_,{by})=> quotes.filter(quote => quote.by === by)
+    },
+    User:{
+        quotes: (parent) => quotes.filter(quote => quote.by === parent.id)
     }
 }
 
