bf1c8794a0a7c1ec261d21a5038373555ac19dee
shreyshah173 <122092519+shreyshah173@users.noreply.github.com>
2024-07-29 23:18:07 +0530

    Add mongodb url

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ import { ApolloServer, gql } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import resolvers from "./resolvers.js";
 import typeDef from "./schemagql.js";
+import dotenv from 'dotenv';
+import mongoose, { mongo } from "mongoose";
+
+dotenv.config();
+
+const url = process.env.mongourl;
+
+mongoose.connect(url);
+
+mongoose.connection.on('open', () => {
+    console.log('Connected to database');
+});
+
+mongoose.connection.on('error', (error) => {
+    console.log('Error connecting to database', error);
+});
 
 const server = new ApolloServer({
     typeDefs: typeDef,
